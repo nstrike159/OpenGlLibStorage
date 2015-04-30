802592754d5653cab9cca162210c773823d8c42d
wwatso1 <wwatso1tech@gmail.com>
2015-04-30 11:58:51 -0500

    Added Map Config

--- 2DShader/src/loader/MapLoader.java
+++ 2DShader/src/loader/MapLoader.java
@@ -1,5 +1,9 @@
 package loader;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 import org.lwjgl.util.vector.Vector2f;
@@ -9,11 +13,33 @@ import render.Tri;
 
 public class MapLoader {
 
-	public static void getMapTris(List<Tri> toAddTo){
-		toAddTo.add(new Tri(new Vector2f(20,20), new Vector2f(50,20), new Vector2f(20,50)));
+	public static void getMapTris(List<Tri> toAddTo) throws FileNotFoundException, IOException{
+		try(BufferedReader br = new BufferedReader(new FileReader("map"))) {
+		    for(String line; (line = br.readLine()) != null; ) {
+		    	if(line.startsWith("t")){
+		    		float[] data = parseData(line);
+		    		toAddTo.add(new Tri(new Vector2f(data[0], data[1]), new Vector2f(data[2], data[3]), new Vector2f(data[4], data[5])));
+		    	}
+		    }
+		}
 	}
 
-	public static void getMapLight(List<Light> toAddTo) {
-		toAddTo.add(new Light(new Vector2f(50, 50), 10, 0, 0));
+	public static void getMapLight(List<Light> toAddTo) throws FileNotFoundException, IOException {
+		try(BufferedReader br = new BufferedReader(new FileReader("map"))) {
+		    for(String line; (line = br.readLine()) != null; ) {
+		    	if(line.startsWith("t")){
+		    		float[] data = parseData(line);
+		    		toAddTo.add(new Light(new Vector2f(data[0], data[1]), data[2], data[3], data[4]));
+		    	}
+		    }
+		}
+	}
+	
+	private static float[] parseData(String s){
+		String dataString = s.substring(s.indexOf('{'), s.indexOf('}'));
+		String[] split = dataString.split(",");
+		float[] data = new float[split.length];
+		System.out.println(dataString);
+		return new float[]{0.0f};
 	}
 }
--- 2DShader/src/main/Main.java
+++ 2DShader/src/main/Main.java
@@ -1,7 +1,6 @@
 package main;
 
-import static org.lwjgl.input.Keyboard.KEY_RETURN;
-import static org.lwjgl.input.Keyboard.isKeyDown;
+import static org.lwjgl.input.Keyboard.*;
 import static org.lwjgl.opengl.GL11.GL_ALWAYS;
 import static org.lwjgl.opengl.GL11.GL_BLEND;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
@@ -33,6 +32,8 @@ import static org.lwjgl.opengl.GL20.glUniform2f;
 import static org.lwjgl.opengl.GL20.glUniform3f;
 import static org.lwjgl.opengl.GL20.glUseProgram;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -78,12 +79,96 @@ public class Main {
 		int shader = ShaderLoader.loadFragmentOnly("frag.glsl");
 		List<Light> lights = new ArrayList<Light>();
 		List<Tri> tris = new ArrayList<>();
-		MapLoader.getMapTris(tris);
-		MapLoader.getMapLight(lights);
+		try {
+			MapLoader.getMapTris(tris);
+			MapLoader.getMapLight(lights);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		List<Block> players = new ArrayList<Block>();
+		Vector2f player = new Vector2f(100, 100);
+		float red = 3.5f,green = 0.0f,blue = 0.0f;
 		while (!Display.isCloseRequested()) {
 			glClear(GL_COLOR_BUFFER_BIT);
 			glUseProgram(shader);
+			if(isKeyDown(KEY_UP))
+				player.y--;
+			if(isKeyDown(KEY_DOWN))
+				player.y++;
+			if(isKeyDown(KEY_LEFT))
+				player.x--;
+			if(isKeyDown(KEY_RIGHT))
+				player.x++;
+			{
+				glColorMask(false, false, false, false);
+				glStencilFunc(GL_ALWAYS, 1, 1);
+				glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+
+				for (Tri block : tris) {
+					Vector2f[] vertices = block.getVertices();
+					for (int i = 0; i < vertices.length; i++) {
+						Vector2f currentVertex = vertices[i];
+						Vector2f nextVertex = vertices[(i + 1) % vertices.length];
+						Vector2f edge = Vector2f.sub(nextVertex, currentVertex, null);
+						Vector2f normal = new Vector2f(edge.getY(), -edge.getX());
+						Vector2f lightToCurrent = Vector2f.sub(currentVertex, player, null);
+						if (Vector2f.dot(normal, lightToCurrent) > 0) {
+							Vector2f point1 = Vector2f.add(currentVertex, (Vector2f) Vector2f.sub(currentVertex, player, null).scale(800), null);
+							Vector2f point2 = Vector2f.add(nextVertex, (Vector2f) Vector2f.sub(nextVertex, player, null).scale(800), null);
+							glBegin(GL_QUADS); {
+								glVertex2f(currentVertex.getX(), currentVertex.getY());
+								glVertex2f(point1.getX(), point1.getY());
+								glVertex2f(point2.getX(), point2.getY());
+								glVertex2f(nextVertex.getX(), nextVertex.getY());
+							} glEnd();
+						}
+					}
+				}
+				
+				for (Block block : players) {
+					Vector2f[] vertices = block.getVertices();
+					for (int i = 0; i < vertices.length; i++) {
+						Vector2f currentVertex = vertices[i];
+						Vector2f nextVertex = vertices[(i + 1) % vertices.length];
+						Vector2f edge = Vector2f.sub(nextVertex, currentVertex, null);
+						Vector2f normal = new Vector2f(edge.getY(), -edge.getX());
+						Vector2f lightToCurrent = Vector2f.sub(currentVertex, player, null);
+						if (Vector2f.dot(normal, lightToCurrent) > 0) {
+							Vector2f point1 = Vector2f.add(currentVertex, (Vector2f) Vector2f.sub(currentVertex, player, null).scale(800), null);
+							Vector2f point2 = Vector2f.add(nextVertex, (Vector2f) Vector2f.sub(nextVertex, player, null).scale(800), null);
+							glBegin(GL_QUADS); {
+								glVertex2f(currentVertex.getX(), currentVertex.getY());
+								glVertex2f(point1.getX(), point1.getY());
+								glVertex2f(point2.getX(), point2.getY());
+								glVertex2f(nextVertex.getX(), nextVertex.getY());
+							} glEnd();
+						}
+					}
+				}
+
+				glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+				glStencilFunc(GL_EQUAL, 0, 1);
+				glColorMask(true, true, true, true);
+
+				glUseProgram(shader);
+				glUniform2f(glGetUniformLocation(shader, "lightLocation"), player.getX(), HEIGHT - player.getY());
+				glUniform3f(glGetUniformLocation(shader, "lightColor"), red, green, blue);
+				glEnable(GL_BLEND);
+				glBlendFunc(GL_ONE, GL_ONE);
+
+				glBegin(GL_QUADS); {
+					glVertex2f(0, 0);
+					glVertex2f(0, HEIGHT);
+					glVertex2f(WIDTH, HEIGHT);
+					glVertex2f(WIDTH, 0);
+				} glEnd();
+
+				glDisable(GL_BLEND);
+				glUseProgram(0);
+				glClear(GL_STENCIL_BUFFER_BIT);
+			}
 			for (Light light : lights) {
 				glColorMask(false, false, false, false);
 				glStencilFunc(GL_ALWAYS, 1, 1);
